508cd7ef86ec6cae5d6631153c26a38b56aac229
JulianMorales <139046283+Onikure@users.noreply.github.com>
2023-10-05 01:16:10 +0800

    Update

--- Assets/SCRIPTS/BULLET.cs
+++ Assets/SCRIPTS/BULLET.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BULLET : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     [Header ("References")]
     [SerializeField] private Rigidbody2D rb;
