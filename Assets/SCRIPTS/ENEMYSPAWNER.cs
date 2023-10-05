01aa3f5f4d3191ff429eac4edb383e627eb8d566
JulianMorales <139046283+Onikure@users.noreply.github.com>
2023-10-05 10:02:39 +0800

    pagod na ko

--- Assets/SCRIPTS/ENEMYSPAWNER.cs
+++ Assets/SCRIPTS/ENEMYSPAWNER.cs
@@ -13,6 +13,7 @@ public class EnemySpawner: MonoBehaviour
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficultyScalingFactor = 0.75f;
+    [SerializeField] private float enemiesPerSecondCap = 15f;
 
     [Header ("Events")]
     public static UnityEvent onEnemyDestory = new UnityEvent();
@@ -21,6 +22,7 @@ public class EnemySpawner: MonoBehaviour
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftToSpawn;
+    private float eps; 
     private bool isSpawning = false;
 
     private void Awake()
@@ -39,7 +41,7 @@ public class EnemySpawner: MonoBehaviour
 
         timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
+        if (timeSinceLastSpawn >= (1f / eps) && enemiesLeftToSpawn > 0)
         {
             SpawnEnemy();
             enemiesLeftToSpawn--;
@@ -63,6 +65,7 @@ public class EnemySpawner: MonoBehaviour
 
         isSpawning = true;
         enemiesLeftToSpawn = EnemiesPerWave();
+        eps = EnemiesPerSecond();
     }
 
     private void EndWave()
@@ -75,12 +78,17 @@ public class EnemySpawner: MonoBehaviour
 
     private void SpawnEnemy()
     {
-        GameObject prefabToSpawn = enemyPrefabs[0];
+        int index = Random.Range (0, enemyPrefabs.Length);
+        GameObject prefabToSpawn = enemyPrefabs[index];
         Instantiate(prefabToSpawn, LevelManager.main.startpoint.position, Quaternion.identity);
     }
 
     private int EnemiesPerWave()
     {
         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+    private float EnemiesPerSecond()
+    {
+        return Mathf.Clamp(enemiesPerSecond * Mathf.Pow(currentWave, difficultyScalingFactor), 0f, enemiesPerSecondCap);
     }
 }
